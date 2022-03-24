76533422ec18e5cad46cd989526a077371c34e26
Dibrary <kkdh828@hanmail.net>
2022-03-24 17:56:23 +0900

    220324 NoReverseMatch Error 해결. blog 애플리케이션 생성

---
+++ bookmark/views.py
@@ -0,0 +1,9 @@
+from bookmark.models import Bookmark
+
+from django.views.generic import ListView, DetailView
+
+class BookmarkLV(ListView):
+    model = Bookmark
+
+class BookmarkDV(DetailView):
+    model = Bookmark
